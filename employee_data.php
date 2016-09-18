344c3c958d31b40885ed21af93c203b20e3b98fc
jennponton1 <jenn.stewart11@gmail.com>
2016-09-17 20:03:33 -0400

    update

--- employee_data.php
+++ employee_data.php
@@ -1,18 +1,34 @@
 <?php
 
-include 'htwapp.class.php';
-require_once 'htframework.inc.php';
+    $list = array (
+        array('Fred', 'Flintstone', 'E45000', '237 Rock Drive', 'Bedrock', 'GA'),
+        array('Betty', 'Rubble', 'E45002', '235 Rock Drive', 'Bedrock', 'GA'),
+        array('Wilma', 'Flintstone', 'E46000', '237 Rock Drive', 'Bedrock', 'GA'),
+        array('Barney', 'Rubble', 'E45003', '235 Rock Drive', 'Bedrock', 'GA'),
+        array('Jennifer', 'Ponton', 'E47000', '123 Somewhere On A Beach Drive', 'Beach City', 'GA')
+    );
 
-function main() {
-    global $htwApp;
-    $tpl = $htwApp->createSmarty();
-    $empfname = 'Jennifer';
-    $tpl->assign('empfname', $empfname);
-    $tpl->display(dirname(__FILE__) . "employee_data.html");
-}
+    $fp = fopen('empfile.csv', 'x+');
 
-$htwApp->addControllerElement('main', 'main');
-$htwApp->dispatch();
+    foreach ($list as $fields) {
+        fputcsv($fp, $fields);
+    }
+
+    fclose($fp);
+
+//include 'htwapp.class.php';
+//require_once 'htframework.inc.php';
+//
+//function main() {
+//    global $htwApp;
+//    $tpl = $htwApp->createSmarty();
+//    $empfname = 'Jennifer';
+//    $tpl->assign('empfname', $empfname);
+//    $tpl->display(dirname(__FILE__) . "employee_data.html");
+//}
+//
+//$htwApp->addControllerElement('main', 'main');
+//$htwApp->dispatch();
 
 
 

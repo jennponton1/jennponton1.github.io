ed7a2dcf11f61f3b7357be6897a0a4133e564ee9
jennponton1 <jenn.stewart11@gmail.com>
2016-09-17 20:07:44 -0400

    update

--- employee_data.php
+++ employee_data.php
@@ -1,20 +1,35 @@
 <?php
 
-    $list = array (
-        array('Fred', 'Flintstone', 'E45000', '237 Rock Drive', 'Bedrock', 'GA'),
-        array('Betty', 'Rubble', 'E45002', '235 Rock Drive', 'Bedrock', 'GA'),
-        array('Wilma', 'Flintstone', 'E46000', '237 Rock Drive', 'Bedrock', 'GA'),
-        array('Barney', 'Rubble', 'E45003', '235 Rock Drive', 'Bedrock', 'GA'),
-        array('Jennifer', 'Ponton', 'E47000', '123 Somewhere On A Beach Drive', 'Beach City', 'GA')
-    );
-
-    $fp = fopen('empfile.csv', 'x+');
-
-    foreach ($list as $fields) {
-        fputcsv($fp, $fields);
+    $row = 1;
+    if (($handle = fopen("empfile.csv", "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $num = count($data);
+            echo "<p> $num fields in line $row: <br /></p>\n";
+            $row++;
+            for ($c=0; $c < $num; $c++) {
+                echo $data[$c] . "<br />\n";
+            }
+        }
+        fclose($handle);
     }
 
-    fclose($fp);
+//    $list = array (
+//        array('Fred', 'Flintstone', 'E45000', '237 Rock Drive', 'Bedrock', 'GA'),
+//        array('Betty', 'Rubble', 'E45002', '235 Rock Drive', 'Bedrock', 'GA'),
+//        array('Wilma', 'Flintstone', 'E46000', '237 Rock Drive', 'Bedrock', 'GA'),
+//        array('Barney', 'Rubble', 'E45003', '235 Rock Drive', 'Bedrock', 'GA'),
+//        array('Jennifer', 'Ponton', 'E47000', '123 Somewhere On A Beach Drive', 'Beach City', 'GA')
+//    );
+//
+//    $fp = fopen('empfile.csv', 'x+');
+//
+//    foreach ($list as $fields) {
+//        fputcsv($fp, $fields);
+//    }
+//
+//    fclose($fp);
+    
+    
 
 //include 'htwapp.class.php';
 //require_once 'htframework.inc.php';

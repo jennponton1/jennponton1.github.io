9309b8fa1807b5177eb34e3733b1c9df08259dcf
jennponton1 <jenn.stewart11@gmail.com>
2016-09-18 20:04:35 -0400

    update

--- employee_data.js
+++ employee_data.js
@@ -1,37 +1,77 @@
-function viewEmps() {
-    $.ajax({
-        type: "GET",
-        url: "empfile.csv",
-        dataType: "text",
-        success: function(data) {processData(data);}
-     });
-//    $.ajax({
-//        type: "GET",
-//        url: "employee_data.php",
-//        success: function() {
-//            
-//        }
-//    });
-}
-
-function processData(allText) {
-    var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(',');
-    var lines = [];
-
-    for (var i=1; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(',');
-        if (data.length == headers.length) {
-
-            var tarr = [];
-            for (var j=0; j<headers.length; j++) {
-                tarr.push(headers[j]+":"+data[j]);
+var employees = {
+    init: function() {
+        
+        $.ajax({
+            type: "GET",
+            url: "empfile.csv",
+            dataType: "text",
+            success: function(data){
+                processData(data);
             }
-            lines.push(tarr);
+        });
+        
+        
+    },
+    getEmps: function() {
+        $.ajax({
+            type: "GET",
+            url: "empfile.csv",
+            dataType: "text",
+            success: function(data) {processData(data);}
+         });
+        //    $.ajax({
+        //        type: "GET",
+        //        url: "employee_data.php",
+        //        success: function() {
+        //            
+        //        }
+        //    });
+    },
+
+    processData: function(allText) {
+        var allTextLines = allText.split(/\r\n|\n/);
+//        var headers = allTextLines[0].split(',');
+        var lines = [];
+
+        for (var i=1; i<allTextLines.length; i++) {
+            var data = allTextLines[i].split(',');
+//            if (data.length == headers.length) {
+
+                var tarr = [];
+                for (var j=0; j<data.length; j++) {
+                    tarr.push(data[j]);
+                    //tarr.push(headers[j]+":"+data[j]);
+                }
+                lines.push(tarr);
+//            }
         }
+         alert(lines);
     }
-     alert(lines);
 }
+
+
+
+$(document).ready(function() {
+    
+    employees.init();
+    
+//    $('#emp_tbl').DataTable( {
+//        data: dataSet,
+//        columns: [
+//            { title: "Name" },
+//            { title: "Position" },
+//            { title: "Office" },
+//            { title: "Extn." },
+//            { title: "Start date" },
+//            { title: "Salary" }
+//        ]
+//    } );
+} );
+
+
+
+
+
 //
 //
 ////window.document.location.href = 'employee_data.php';

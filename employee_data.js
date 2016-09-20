c4b8e0b86e9b193b7cb3624e488333ed67a6fdad
jennponton1 <jenn.stewart11@gmail.com>
2016-09-20 08:05:34 -0400

    u

--- employee_data.js
+++ employee_data.js
@@ -2,6 +2,11 @@
 
 $(document).ready(function() {
     
+    $.ajax({
+        url: 'empfile.csv',
+        dataType: 'text',
+    }).done(successFunction);
+    
 //    $.ajax({
 //        type: "GET",
 //        url: "employee_data.php",
@@ -11,22 +16,57 @@ $(document).ready(function() {
 //        
 //    });
     
-    //function processData(csv) {
-        csv = "empfile.csv";
-        var allTextLines = csv.split(/\r\n|\n/);
-        var lines = [];
-        for (var i=0; i<allTextLines.length; i++) {
-            var data = allTextLines[i].split(';');
-                var tarr = [];
-                for (var j=0; j<data.length; j++) {
-                    tarr.push(data[j]);
-                }
-                lines.push(tarr);
-        }
-      console.log(lines);
+//    //function processData(csv) {
+//        csv = "empfile.csv";
+//        var allTextLines = csv.split(/\r\n|\n/);
+//        var lines = [];
+//        for (var i=0; i<allTextLines.length; i++) {
+//            var data = allTextLines[i].split(';');
+//                var tarr = [];
+//                for (var j=0; j<data.length; j++) {
+//                    tarr.push(data[j]);
+//                }
+//                lines.push(tarr);
+//        }
+//      console.log(lines);
 //    }
     
 } );
+
+function successFunction(data) {
+  var allRows = data.split(/\r?\n|\r/);
+  var table = '<table>';
+  for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+    if (singleRow === 0) {
+      table += '<thead>';
+      table += '<tr>';
+    } else {
+      table += '<tr>';
+    }
+    var rowCells = allRows[singleRow].split(',');
+    for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+      if (singleRow === 0) {
+        table += '<th>';
+        table += rowCells[rowCell];
+        table += '</th>';
+      } else {
+        table += '<td>';
+        table += rowCells[rowCell];
+        table += '</td>';
+      }
+    }
+    if (singleRow === 0) {
+      table += '</tr>';
+      table += '</thead>';
+      table += '<tbody>';
+    } else {
+      table += '</tr>';
+    }
+  } 
+  table += '</tbody>';
+  table += '</table>';
+  $('body').append(table);
+}
 
 
 
